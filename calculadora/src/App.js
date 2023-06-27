50a0eff6d5cff7c37a17faf5aa715fd67cba0132
Valentina-Assef <valenassef@gmail.com>
2023-06-27 18:45:36 -0300

    Principal structure

--- calculadora/src/App.js
+++ calculadora/src/App.js
@@ -1,9 +1,48 @@
 import './App.css';
+import logo from './imagenes/logo.png';
+import Boton from './componentes/Boton';
+import Pantalla from './componentes/Pantalla';
+import BotonClear from './componentes/BotonClear';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className='App'>
+      <div className='logo-contenedor'>
+        <img 
+          src={logo}
+          className='logo'
+          alt='Logo' />
+      </div>
+      <div className='calculadora-contenedor'>
+        <Pantalla />
+        <div className='fila'>
+          <Boton>1</Boton>
+          <Boton>2</Boton>
+          <Boton>3</Boton>
+          <Boton>+</Boton>
+        </div>
+        <div className='fila'>
+          <Boton>4</Boton>
+          <Boton>5</Boton>
+          <Boton>6</Boton>
+          <Boton>-</Boton>
+        </div>
+        <div className='fila'>
+          <Boton>7</Boton>
+          <Boton>8</Boton>
+          <Boton>9</Boton>
+          <Boton>*</Boton>
+        </div>
+        <div className='fila'>
+          <Boton>=</Boton>
+          <Boton>0</Boton>
+          <Boton>.</Boton>
+          <Boton>/</Boton>
+        </div>
+        <div className='fila'>
+          <BotonClear>Clear</BotonClear>
+        </div>
+      </div>
     </div>
   );
 }
