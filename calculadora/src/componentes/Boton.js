6049c4c45a5bf0cafa6b194e25728e14048295c4
Valentina-Assef <valenassef@gmail.com>
2023-06-27 19:59:49 -0300

    Basic Functionality

--- calculadora/src/componentes/Boton.js
+++ calculadora/src/componentes/Boton.js
@@ -4,11 +4,12 @@ import '../styles/Boton.css';
 function Boton(props) {
 
   const esOperador = valor => {
-    return isNaN(valor) && (valor != '.') && (valor != '=');
+    return isNaN(valor) && (valor !== '.') && (valor !== '=');
   };
 
   return (
-    <div className={`boton-contenedor ${esOperador(props.children) ? 'operador' : ''}`}>
+    <div className={`boton-contenedor ${esOperador(props.children) ? 'operador' : ''}`}
+    onClick={() => props.manejarClic(props.children)}>
       {props.children}
     </div>
   );
